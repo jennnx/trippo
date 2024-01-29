3aac031ddc245e766204c0b73a97c3e5c14951e4
Jennnx <jenlee2617@gmail.com>
2024-01-29 12:19:00 -0800

    Changed dashboard text

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,7 +2,7 @@ export default function Dashboard() {
     return (
         <div>
             <h1 className="text-xl">Dashboard Page</h1>
-            <p>DashDashDash</p>
+            <p>This is the new and improved page!</p>
         </div>
     );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,15 +8,3 @@ export default function Navbar() {
         </nav>
     );
 }
-
-/**
- * 1. Home page ("/")
- * 2. Dashboard page ("/dashboard")
- * 3. Discover page ("/discover")
- *
- * Your assignment:
- * 1. Add the discover page
- * 2. Add navigation in between the dashboard and the discover page
- *      1. When the user clicks Trippo -> takes them to dashboard
- *      2. When the user clicks Discover -> takes them to discover
- */
